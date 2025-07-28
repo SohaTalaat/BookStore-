55a7e7fc27fdf29d006f588a0cc5a17058e692fa
Mustafa-Hamdi <mustafahamdi116@gmail.com>
2025-07-28 12:39:04 +0300

    final editting in shaa allah

--- js/shop.js
+++ js/shop.js
@@ -1,20 +1,14 @@
 let booksGrid = document.getElementById("booksGrid");
 let booksGrid2 = document.getElementById("booksGrid2");
 let addToCart;
-let deleteFromCart;
 let bookInfo;
 let book;
-//  Event Listeners
-
 document.addEventListener("DOMContentLoaded", loadNew);
 document.addEventListener("DOMContentLoaded", loadBest);
 document.addEventListener("click", addToLocalStorage);
 document.addEventListener("click", showBookInfo);
-// viewAll.addEventListener("click", loadMoreBooks);
-// function loadMoreBooks() {}
-
-// Extract API Data
-function loadNew() {
+                                                                                 
+function loadNew() {                                                                                              // Extract API Data
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -22,7 +16,7 @@ function loadNew() {
       displayNew(book);
     }
   };
-  var url = "https://openlibrary.org/search.json?q=new+releases&limit=9";
+  var url = "https://openlibrary.org/search.json?q=new+releases&limit=8";
   xhr.open("get", url, true);
   xhr.send();
 }
@@ -38,7 +32,6 @@ function displayNew(p) {
     var coverId = jsonData.docs[i].cover_i;
     bookImage.src = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
     bookCard.appendChild(bookImage);
-
     // Create Info Card
     let bookInfo = document.createElement("div");
     bookInfo.className = "book-info";
@@ -48,32 +41,20 @@ function displayNew(p) {
     let bookAuthor = document.createElement("p");
     bookAuthor.className = "book-author";
     bookAuthor.innerText = `By ${jsonData.docs[i].author_name}`;
-
     // Create Spacer
     let spacer = document.createElement("div");
     spacer.className = "spacer";
-
     //Add To Cart Button
     let addToCart = document.createElement("button");
     addToCart.className = "add-cart";
     addToCart.innerText = "Add To Cart";
-    //delate from Cart Button
-    let deleteFromCart = document.createElement("button");
-    deleteFromCart.className = "add-cart disabled";
-    deleteFromCart.innerText = "delate from Cart";
-
   // check if book checked befor reload
-
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let coverIdStr = String(coverId); 
-
     if (cart.some((book) => book.id === coverIdStr)) {
       addToCart.innerText = "Added to Cart";
       addToCart.disabled = true;
       addToCart.classList.add("disabled");
-      deleteFromCart.innerText = "delate from Cart";
-      deleteFromCart.disabled = false;
-      deleteFromCart.className = "add-cart";
     }
     // Append Data
     bookInfo.appendChild(bookTitle);
@@ -81,22 +62,18 @@ function displayNew(p) {
     bookCard.appendChild(bookInfo);
     bookInfo.appendChild(spacer);
     bookInfo.appendChild(addToCart);
-    bookInfo.appendChild(deleteFromCart);
     booksGrid2.append(bookCard);
-  }
-}
+  }}
 // BestSellers Section
 // Extract API Data
-
 function loadBest() {
   var xhr2 = new XMLHttpRequest();
   xhr2.onreadystatechange = function () {
     if (xhr2.readyState == 4 && xhr2.status == 200) {
       var book = xhr2.responseText;
       displayBest(book);
-    }
-  };
-  var url = "https://openlibrary.org/search.json?q=bestsellers&limit=12";
+    }};
+  var url = "https://openlibrary.org/search.json?q=bestsellers&limit=20";
   xhr2.open("get", url, true);
   xhr2.send();
 }
@@ -112,7 +89,6 @@ function displayBest(p) {
     var coverId = jsonData.docs[i].cover_i;
     bookImage.src = `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
     bookCard.appendChild(bookImage);
-
     // Create Info Card
     let bookInfo = document.createElement("div");
     bookInfo.className = "book-info";
@@ -122,32 +98,20 @@ function displayBest(p) {
     let bookAuthor = document.createElement("p");
     bookAuthor.className = "book-author";
     bookAuthor.innerText = `By ${jsonData.docs[i].author_name}`;
-
     // Create Spacer
     let spacer = document.createElement("div");
     spacer.className = "spacer";
-
     //Add To Cart Button
     let addToCart = document.createElement("button");
     addToCart.className = "add-cart";
     addToCart.innerText = "Add To Cart";
-
-    //delate from Cart Button
-    let deleteFromCart = document.createElement("button");
-    deleteFromCart.className = "add-cart disabled";
-    deleteFromCart.innerText = "delate from Cart";
-
     // check if book checked befor reload
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let coverIdStr = String(coverId); 
-
     if (cart.some((book) => book.id === coverIdStr)) {
       addToCart.innerText = "Added to Cart";
       addToCart.disabled = true;
       addToCart.classList.add("disabled");
-      deleteFromCart.innerText = "delate from Cart";
-      deleteFromCart.disabled = false;
-      deleteFromCart.className = "add-cart";
     }
     // Append Data
     bookInfo.appendChild(bookTitle);
@@ -155,20 +119,15 @@ function displayBest(p) {
     bookCard.appendChild(bookInfo);
     bookInfo.appendChild(spacer);
     bookInfo.appendChild(addToCart);
-    bookInfo.appendChild(deleteFromCart);
     booksGrid.append(bookCard);
-  }
-}
-
-//  Event functions
-function addToLocalStorage(e) {
+  }}
+ //Event functions
+  function addToLocalStorage(e) {
   if (e.target.classList.contains("add-cart")) {
     const bookCard = e.target.closest(".book-card");
     const title = bookCard.querySelector(".book-title").innerText;
     const imageSrc = bookCard.querySelector(".book-cover").src;
-
     const coverId = imageSrc.match(/\/b\/id\/(\d+)-L\.jpg/)[1];
-
     const newBook = {
       title: title,
       image: imageSrc,
@@ -177,11 +136,9 @@ function addToLocalStorage(e) {
 
     // // Initialize Array
     let cart = [];
-
     if (localStorage.getItem("cart")) {
       cart = JSON.parse(localStorage.getItem("cart"));
-    }
-
+    };
     const exists = cart.some((book) => book.id === newBook.id);
     if (!exists) {
       cart.push(newBook);
@@ -190,11 +147,8 @@ function addToLocalStorage(e) {
       e.target.innerText = "Added To Cart";
       e.target.disabled = true;
       e.target.classList.add("disabled");
-    }
-  }
-}
-
-function showBookInfo(e) {
+  }};
+  function showBookInfo(e) {
   if (e.target.classList.contains("book-cover")) {
     const bookCard = e.target.closest(".book-card");
     const title = bookCard.querySelector(".book-title").innerText;
@@ -207,5 +161,4 @@ function showBookInfo(e) {
     );
     // Single book page
     window.location.href = "singleBookPage.html";
-  }
-}
+  }}}
